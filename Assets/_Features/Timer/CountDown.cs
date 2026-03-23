a34a321b117c3b9772458bcaee262bc575c20b04
yuu30024 <arcobaleno2991@gmail.com>
2026-03-23 17:59:52 +0900

    feature: タイマーを追加
    Issue: 19

---
+++ Assets/_Features/Timer/CountDown.cs
@@ -0,0 +1,77 @@
+using TMPro;
+
+using UnityEngine;
+
+namespace Uraty.Feature.Timer
+{
+    public class CountDown : MonoBehaviour
+    {
+        [SerializeField] private float _durationSeconds = 60f;
+        [SerializeField] private TextMeshProUGUI _timerText;
+
+        private float _remainingSeconds;
+        private bool _isRunning;
+
+        public bool IsRunning => _isRunning;
+        public float RemainingSeconds => _remainingSeconds;
+
+        private void Awake()
+        {
+            _remainingSeconds = _durationSeconds;
+            _isRunning = false;
+        }
+
+        private void Start()
+        {
+            StartTimer();
+        }
+
+        private void Update()
+        {
+            if (!_isRunning)
+            {
+                return;
+            }
+
+            if (_remainingSeconds > 0f)
+            {
+                _remainingSeconds -= Time.deltaTime;
+                UpdateTimerDisplay(_remainingSeconds);
+                return;
+            }
+
+            _remainingSeconds = 0f;
+            _isRunning = false;
+            HandleTimerCompleted();
+        }
+
+        public void StartTimer()
+        {
+            _isRunning = true;
+        }
+
+        public void StopTimer()
+        {
+            _isRunning = false;
+        }
+
+        public void ResetTimer()
+        {
+            _remainingSeconds = _durationSeconds;
+            UpdateTimerDisplay(_remainingSeconds);
+        }
+
+        private void UpdateTimerDisplay(float remainingSeconds)
+        {
+            var minutes = Mathf.FloorToInt(remainingSeconds / 60f);
+            var seconds = Mathf.FloorToInt(remainingSeconds % 60f);
+
+            _timerText.text = $"{minutes:00}:{seconds:00}";
+        }
+
+        private void HandleTimerCompleted()
+        {
+            Debug.Log("Timer completed");
+        }
+    }
+}
